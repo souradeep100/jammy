f02451adf0af27fa0303a66fdd871388b2ed6f8d
Tim Gardner <tim.gardner@canonical.com>
2022-09-22 12:59:03 -0600

    Revert "PCI: hv: Fix interrupt mapping for multi-MSI"
    This reverts commit 1c424ab23bb0ecb2dd7444f328e69e7f0ea7aed5.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/pci/controller/pci-hyperv.c
+++ drivers/pci/controller/pci-hyperv.c
@@ -1142,10 +1142,6 @@ static void hv_int_desc_free(struct hv_pci_dev *hpdev,
 		u8 buffer[sizeof(struct pci_delete_interrupt)];
 	} ctxt;
 
-	if (!int_desc->vector_count) {
-		kfree(int_desc);
-		return;
-	}
 	memset(&ctxt, 0, sizeof(ctxt));
 	int_pkt = (struct pci_delete_interrupt *)&ctxt.pkt.message;
 	int_pkt->message_type.type =
@@ -1206,13 +1202,6 @@ static int hv_set_affinity(struct irq_data *data, const struct cpumask *dest,
 static void hv_irq_mask(struct irq_data *data)
 {
 	pci_msi_mask_irq(data);
-}
-
-static unsigned int hv_msi_get_int_vector(struct irq_data *data)
-{
-	struct irq_cfg *cfg = irqd_cfg(data);
-
-	return cfg->vector;
 }
 
 static int hv_msi_prepare(struct irq_domain *domain, struct device *dev,
@@ -1370,12 +1359,12 @@ static void hv_pci_compose_compl(void *context, struct pci_response *resp,
 
 static u32 hv_compose_msi_req_v1(
 	struct pci_create_interrupt *int_pkt, struct cpumask *affinity,
-	u32 slot, u8 vector, u8 vector_count)
+	u32 slot, u8 vector)
 {
 	int_pkt->message_type.type = PCI_CREATE_INTERRUPT_MESSAGE;
 	int_pkt->wslot.slot = slot;
 	int_pkt->int_desc.vector = vector;
-	int_pkt->int_desc.vector_count = vector_count;
+	int_pkt->int_desc.vector_count = 1;
 	int_pkt->int_desc.delivery_mode = APIC_DELIVERY_MODE_FIXED;
 
 	/*
@@ -1398,14 +1387,14 @@ static int hv_compose_msi_req_get_cpu(struct cpumask *affinity)
 
 static u32 hv_compose_msi_req_v2(
 	struct pci_create_interrupt2 *int_pkt, struct cpumask *affinity,
-	u32 slot, u8 vector, u8 vector_count)
+	u32 slot, u8 vector)
 {
 	int cpu;
 
 	int_pkt->message_type.type = PCI_CREATE_INTERRUPT_MESSAGE2;
 	int_pkt->wslot.slot = slot;
 	int_pkt->int_desc.vector = vector;
-	int_pkt->int_desc.vector_count = vector_count;
+	int_pkt->int_desc.vector_count = 1;
 	int_pkt->int_desc.delivery_mode = APIC_DELIVERY_MODE_FIXED;
 	cpu = hv_compose_msi_req_get_cpu(affinity);
 	int_pkt->int_desc.processor_array[0] =
@@ -1417,7 +1406,7 @@ static u32 hv_compose_msi_req_v2(
 
 static u32 hv_compose_msi_req_v3(
 	struct pci_create_interrupt3 *int_pkt, struct cpumask *affinity,
-	u32 slot, u32 vector, u8 vector_count)
+	u32 slot, u32 vector)
 {
 	int cpu;
 
@@ -1425,7 +1414,7 @@ static u32 hv_compose_msi_req_v3(
 	int_pkt->wslot.slot = slot;
 	int_pkt->int_desc.vector = vector;
 	int_pkt->int_desc.reserved = 0;
-	int_pkt->int_desc.vector_count = vector_count;
+	int_pkt->int_desc.vector_count = 1;
 	int_pkt->int_desc.delivery_mode = APIC_DELIVERY_MODE_FIXED;
 	cpu = hv_compose_msi_req_get_cpu(affinity);
 	int_pkt->int_desc.processor_array[0] =
@@ -1448,6 +1437,7 @@ static u32 hv_compose_msi_req_v3(
  */
 static void hv_compose_msi_msg(struct irq_data *data, struct msi_msg *msg)
 {
+	struct irq_cfg *cfg = irqd_cfg(data);
 	struct hv_pcibus_device *hbus;
 	struct vmbus_channel *channel;
 	struct hv_pci_dev *hpdev;
@@ -1456,8 +1446,6 @@ static void hv_compose_msi_msg(struct irq_data *data, struct msi_msg *msg)
 	struct cpumask *dest;
 	struct compose_comp_ctxt comp;
 	struct tran_int_desc *int_desc;
-	struct msi_desc *msi_desc;
-	u8 vector, vector_count;
 	struct {
 		struct pci_packet pci_pkt;
 		union {
@@ -1479,8 +1467,7 @@ static void hv_compose_msi_msg(struct irq_data *data, struct msi_msg *msg)
 		return;
 	}
 
-	msi_desc  = irq_data_get_msi_desc(data);
-	pdev = msi_desc_to_pci_dev(msi_desc);
+	pdev = msi_desc_to_pci_dev(irq_data_get_msi_desc(data));
 	dest = irq_data_get_effective_affinity_mask(data);
 	pbus = pdev->bus;
 	hbus = container_of(pbus->sysdata, struct hv_pcibus_device, sysdata);
@@ -1493,36 +1480,6 @@ static void hv_compose_msi_msg(struct irq_data *data, struct msi_msg *msg)
 	if (!int_desc)
 		goto drop_reference;
 
-	if (!msi_desc->msi_attrib.is_msix && msi_desc->nvec_used > 1) {
-		/*
-		 * If this is not the first MSI of Multi MSI, we already have
-		 * a mapping.  Can exit early.
-		 */
-		if (msi_desc->irq != data->irq) {
-			data->chip_data = int_desc;
-			int_desc->address = msi_desc->msg.address_lo |
-					    (u64)msi_desc->msg.address_hi << 32;
-			int_desc->data = msi_desc->msg.data +
-					 (data->irq - msi_desc->irq);
-			msg->address_hi = msi_desc->msg.address_hi;
-			msg->address_lo = msi_desc->msg.address_lo;
-			msg->data = int_desc->data;
-			put_pcichild(hpdev);
-			return;
-		}
-		/*
-		 * The vector we select here is a dummy value.  The correct
-		 * value gets sent to the hypervisor in unmask().  This needs
-		 * to be aligned with the count, and also not zero.  Multi-msi
-		 * is powers of 2 up to 32, so 32 will always work here.
-		 */
-		vector = 32;
-		vector_count = msi_desc->nvec_used;
-	} else {
-		vector = hv_msi_get_int_vector(data);
-		vector_count = 1;
-	}
-
 	memset(&ctxt, 0, sizeof(ctxt));
 	init_completion(&comp.comp_pkt.host_event);
 	ctxt.pci_pkt.completion_func = hv_pci_compose_compl;
@@ -1533,8 +1490,7 @@ static void hv_compose_msi_msg(struct irq_data *data, struct msi_msg *msg)
 		size = hv_compose_msi_req_v1(&ctxt.int_pkts.v1,
 					dest,
 					hpdev->desc.win_slot.slot,
-					vector,
-					vector_count);
+					cfg->vector);
 		break;
 
 	case PCI_PROTOCOL_VERSION_1_2:
@@ -1542,16 +1498,14 @@ static void hv_compose_msi_msg(struct irq_data *data, struct msi_msg *msg)
 		size = hv_compose_msi_req_v2(&ctxt.int_pkts.v2,
 					dest,
 					hpdev->desc.win_slot.slot,
-					vector,
-					vector_count);
+					cfg->vector);
 		break;
 
 	case PCI_PROTOCOL_VERSION_1_4:
 		size = hv_compose_msi_req_v3(&ctxt.int_pkts.v3,
 					dest,
 					hpdev->desc.win_slot.slot,
-					vector,
-					vector_count);
+					cfg->vector);
 		break;
 
 	default:
